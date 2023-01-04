656a67e5d911f0d32b42c897554843c3588fefdc
Brian Mutai <45977755+bryan1239@users.noreply.github.com>
2023-01-04 22:42:46 +0300

    Add files via upload

---
+++ active ports.py
@@ -0,0 +1,35 @@
+import socket
+import time
+
+ip = "197.232.88.9"
+port = 80
+retry = 5
+delay = 10
+timeout = 3
+
+def isOpen(ip, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        try:
+                s.connect((ip, int(port)))
+                s.shutdown(socket.SHUT_RDWR)
+                return True
+        except:
+                return False
+        finally:
+                s.close()
+
+def checkHost(ip, port):
+        ipup = False
+        for i in range(retry):
+                if isOpen(ip, port):
+                        ipup = True
+                        break
+                else:
+                        time.sleep(delay)
+        return ipup
+
+if checkHost(ip, port):
+        print (ip + " is UP")
+else:
+        print (ip + " is DOWN")
